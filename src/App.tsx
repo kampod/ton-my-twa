85eb9bedb3e5ab48e97260bc556168a8f1726827
kampod <v.kinzeriavyy@gmail.com>
2024-04-29 19:38:49 +0200

    Base setup

---
+++ src/App.tsx
@@ -0,0 +1,51 @@
+import './App.css';
+import { TonConnectButton } from '@tonconnect/ui-react';
+import { useTonConnect } from './hooks/useTonConnect';
+import { useCounterContract } from './hooks/useCounterContract';
+
+function App() {
+    const { connected } = useTonConnect();
+    const { value, address, sendIncrement, sendDecrement } = useCounterContract();
+
+    return (
+        <div className='App'>
+            <div className='Container'>
+                <TonConnectButton />
+
+                <div className='Card'>
+                    <b>Counter Address</b>
+                    <div className='Hint'>{address?.slice(0, 30) + '...'}</div>
+                </div>
+
+                <div className='Card'>
+                    <b>Counter Value</b>
+                    <div>{value ?? 'Loading...'}</div>
+                </div>
+
+                <div className='button-increment'>
+                    <a
+                        className={`Button ${connected ? 'Active' : 'Disabled'}`}
+                        onClick={() => {
+                            sendIncrement();
+                        }}
+                    >
+                        Increment
+                    </a>
+                </div>
+
+                <div className='button-decrement'>
+                    <a
+                        className={`Button ${connected ? 'Active' : 'Disabled'}`}
+                        onClick={() => {
+                            sendDecrement();
+                        }}
+                    >
+                        Decrement
+                    </a>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default App
